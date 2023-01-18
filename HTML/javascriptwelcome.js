58fe68d3a3097fc00640f06fea7abc59d0616c02
anjalipa2001 <120706917+anjalipa2001@users.noreply.github.com>
2023-01-18 11:50:00 +0530

    Add files via upload

---
+++ HTML/javascriptwelcome.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<script>
+function.sayhello()
+{
+alert("Hello world")
+}
+</script>
+</head>
+<body>
+<input type="button" onclick="sayhello()" value="Say Hello"/>
+</body>
+</html>
